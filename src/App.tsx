a84067af13a63b71aaaa93b32150b3c021ff12b7
TalalBnoob <TalalBnoobBTW@gmail.com>
2022-09-28 19:47:09 +0300

    repo init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<h1 className="App bg-gray-600 text-blue-500 text-6xl underline flex justify-center items-center h-40">
+			Hello World!
+		</h1>
+	);
+}
+
+export default App;
